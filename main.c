105269fe6ed2764ef0ba6499f26530e8f8a84e4e
gayoung46 <gayoung04066@naver.com>
2025-10-16 15:27:26 +0900

    07_2

--- main.c
+++ main.c
@@ -3,16 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    int flag = 1;
-    int y;
+    int i ;
     
-    while(flag !=0)
+    for(i=0;i<5;i++)
     {
-               int y;
-               y = 3;
-               flag = 0;
+                    static int temp = 1;//static int하면 소멸되지 않고 생존기간이 계속됨 
+                    printf("temp = %d\n",temp);
+                    temp++;
     }
-    y = 4;
     
   system("PAUSE");	
   return 0;
