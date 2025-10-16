056df2db2b059fa5c9ac3793b64c0b00205a62e3
gayoung46 <gayoung04066@naver.com>
2025-10-16 15:58:21 +0900

    07_5

--- main.c
+++ main.c
@@ -9,24 +9,29 @@
   //return 0;
 //}
 
-void f(void);
-
-//int i;
+void sub(void);
 
 int main(void)
 {
-    int i; 
-    for(i=0;i<5;i++)
+    int i;
+    for(i=0;i<3;i++)
     {
-                    f();
+                    sub();
     }
+    
     system("PAUSE");
     return 0;
 }
 
-void f(void)
+void sub(void)
 {
-     int i;
-     for(i=0;i<10;i++)
-     printf("#"); 
+     int auto_count = 0;
+     static int static_count = 0;
+     
+     auto_count++;
+     static_count++;
+     
+     printf("auto_count=%d\n", auto_count);
+     printf("static_count=%d\n", static_count);
+     
 }
