b3710b0ceffef9c49d4627ec9b9495af849477cc
gayoung46 <gayoung04066@naver.com>
2025-10-16 15:49:03 +0900

    07_4

--- main.c
+++ main.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inc(int counter);
+//int main(int argc, char *argv[])
+//{
+    
+    
+  //system("PAUSE");	
+  //return 0;
+//}
+
+void f(void);
+
+//int i;
 
 int main(void)
 {
-    int i = 10;
-    
-    printf("함수 호출전 i=%d\n", i);
-    i = inc(i);//함수의 반환값을 사용, 반환되는 값을 왼쪽에서 받아줌 
-    //inc(i);를 하면 반환값을 버리는 것  
-    printf("함수 호출후 i=%d\n", i); 
-    
-  system("PAUSE");	
-  return 0;
+    int i; 
+    for(i=0;i<5;i++)
+    {
+                    f();
+    }
+    system("PAUSE");
+    return 0;
 }
 
-int inc(int counter)//매개변수는 함수가 없어지면 값이 끝남  
+void f(void)
 {
-    counter++;
-    return counter;
+     int i;
+     for(i=0;i<10;i++)
+     printf("#"); 
 }
-
-//int main(int argc, char *argv[])
